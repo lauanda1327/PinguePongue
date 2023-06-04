71c411dc89fb0b9ac18afbf2b151d2051fe4b46c
Lauanda Nobre <113943598+lauanda1327@users.noreply.github.com>
2023-06-04 16:38:46 -0300

    Update victory.js

--- patriot_game/victory.js
+++ patriot_game/victory.js
@@ -1,6 +1,6 @@
 function victory() {
 	if(pontos === 5) {
-		alert('jogador venceu, clique em ok para jogar novamente')
+		alert('Você venceu, clique em ok para jogar novamente')
 		pontos = 0
 		pontosOponent = 0
 	}if(pontosOponent === 5) {
